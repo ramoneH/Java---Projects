a3da45dd695bbf366f76c08cf88f1dd6dff5fd1b
javaclass <javaclass@15000533cpu.fec.org>
2017-10-04 13:31:15 -0500

    FOR LOOPS & STRINGS

---
+++ src/Learn/BobWelcomes.java
@@ -0,0 +1,17 @@
+package Learn;
+
+public class BobWelcomes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	String[] h = {" Welcome To The Class"};
+    String[] turtlesGo = {"Michaelangelo","Donatello","Raphael","Leonardo", };
+    	for(int n=0;n<turtlesGo.length;n++){
+    		
+    		System.out.println(turtlesGo + " " + h);
+    	}
+    
+    
+	}
+
+}
