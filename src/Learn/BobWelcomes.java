36f5bdcea0307ed318891eec070ef26cac0558ad
javaclass <javaclass@15000533cpu.fec.org>
2017-10-05 15:56:03 -0500

    aNOTHER dAY

--- src/Learn/BobWelcomes.java
+++ src/Learn/BobWelcomes.java
@@ -4,14 +4,13 @@ public class BobWelcomes {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	String[] h = {" Welcome To The Class"};
     String[] turtlesGo = {"Michaelangelo","Donatello","Raphael","Leonardo", };
-    	for(int n=0;n<turtlesGo.length;n++){
+    	for(String name : turtlesGo )
     		
-    		System.out.println(turtlesGo + " " + h);
+    		System.out.println(name + "- welcome to the class ");
     	}
     
     
 	}
 
-}
+
---
+++ src/Learn/PasswordCheck.java
@@ -0,0 +1,34 @@
+package Learn;
+
+import java.util.Scanner;
+
+public class PasswordCheck {
+	
+	public static void main(String[] args) {
+		
+		//String pass = "ram1234one";
+		
+		int numcount =0;
+		int charcount =0;
+		
+		System.out.println(" enter a password");
+		
+		String input = new Scanner(System.in).nextLine();
+		
+					
+		
+			
+			if (input.charAt(p)>='A' && input.charAt(p)<='Z') {
+				
+				;
+			}
+			else if (input.charAt(p)>='A' && input.charAt(p)<='Z') {
+				if(Character.isLetter(input.charAt(p)));
+				p++;
+	}
+		//for(digit < input.length; );
+	}   //char p = input.charAt(0);
+		//int digit =0;
+		
+}
+	
